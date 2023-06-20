50e541bd7745b17f95df5eae86c992cec0fec244
Eskaine <eskaineliu@gmail.com>
2023-06-20 15:18:28 +0800

    chore: added background pattern to body in client

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -25,6 +25,17 @@ const theme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
   },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background: 'radial-gradient(circle, transparent 20%, #ECF0F1 20%, #ECF0F1 80%, transparent 80%, transparent), radial-gradient(circle, transparent 20%, #ECF0F1 20%, #ECF0F1 80%, transparent 80%, transparent) 35px 35px, linear-gradient(#BDC3C7 2.8000000000000003px, transparent 2.8000000000000003px) 0 -1.4000000000000001px, linear-gradient(90deg, #BDC3C7 2.8000000000000003px, #ECF0F1 2.8000000000000003px) -1.4000000000000001px 0',
+          backgroundColor: '#ECF0F1',
+          backgroundSize: '70px 70px, 70px 70px, 35px 35px, 35px 35px',
+        },
+      }
+    }
+  }
 });
 
 export default theme;
